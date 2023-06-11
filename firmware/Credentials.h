5adff840b144a76dcc43443600e7c1cda7c14091
Marc-Oliver Blumenauer <122913485+TheDiyDude@users.noreply.github.com>
2023-06-11 22:29:45 +0200

    added MQTT support

--- firmware/Credentials.h
+++ firmware/Credentials.h
@@ -7,3 +7,15 @@ const char* password = "_MYWIFI_PASSWORD_";
 const char* ota_password = "pd2023";
 // Hostname defaults to esp8266-[ChipID]
 const char* ota_hostname = "PerfectDraft";
+
+// MQTT Setup
+// Uncomment the define __MQTT_ENABLED__ to enable MQTT implementation
+// and ensure to setup MQTT credentials ...
+//
+//#define __MQTT_ENABLED__
+#if defined(__MQTT_ENABLED__)
+  const char* mqtt_hostname = "_BROKER_";
+  #define MQTT_PORT 1886
+  const char* mqtt_username = "_BROKER_USER_";
+  const char* mqtt_password = "_BROKER_PASSWORD_";
+#endif
