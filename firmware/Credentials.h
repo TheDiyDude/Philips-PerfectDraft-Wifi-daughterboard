ca962ac56c761ac9d1f20505df5a78f083de2c55
Marc-Oliver Blumenauer <122913485+TheDiyDude@users.noreply.github.com>
2023-06-14 19:40:20 +0200

    added MQTT and cleanup Wifi Setup AP Mode

--- firmware/Credentials.h
+++ firmware/Credentials.h
@@ -1,7 +1,3 @@
-// Add your WIFI credentials here
-const char* ssid = "_MYWIFI_SSID_";
-const char* password = "_MYWIFI_PASSWORD_";
-
 // OTA Auth Setup
 // No authentication by default
 const char* ota_password = "pd2023";
